47afd759ab849c6b8f5320044b49834c0d3cb855
qwe8851 <dgh07027@naver.com>
2023-09-06 00:18:55 +0900

    feat: 여러 이벤트 수식어
    .prevent → preventDefault
    .left/.rignt/ .middle → 마우스
    .enter / .shift / .page-down

--- basics-assignment-1-problem/basics-03-events-starting-code/app.js
+++ basics-assignment-1-problem/basics-03-events-starting-code/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      confirmedName: "",
     };
   },
   methods: {
@@ -14,6 +15,13 @@ const app = Vue.createApp({
     },
     setName(e, lastName) {
       this.name = e.target.value + ` ${lastName}`;
+    },
+    confirmInput(){
+      this.confirmedName = this.name;
+    },
+    submitForm(e){
+      // e.preventDefault();
+      alert("Submitted!");
     },
   },
 });
