52864b7f770fb9b6566d5c1bc13182d1a5343e21
qwe8851 <dgh07027@naver.com>
2023-09-05 22:59:36 +0900

    feat: 네이티브 $event 객체

--- basics-assignment-1-problem/basics-03-events-starting-code/app.js
+++ basics-assignment-1-problem/basics-03-events-starting-code/app.js
@@ -2,7 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
+  },
+  methods: {
+    add(num) {
+      this.counter += num;
+    },
+    reduce() {
+      this.counter--;
+    },
+    setName(e, lastName) {
+      this.name = e.target.value + ` ${lastName}`;
+    },
   },
 });
 
