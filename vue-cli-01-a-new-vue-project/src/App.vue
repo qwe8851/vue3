500b3fefc1a9a7c1f7b907e8d36a8d7ef609abc6
qwe8851 <dgh07027@naver.com>
2023-09-09 17:57:58 +0900

    feat: 기본 Vue CLI app 추가

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<section>
+    <h2>My Friends</h2>
+    <ul>
+        <li></li>
+    </ul>
+</section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '1234 5678 90',
+                    email: 'manuel@localhost.com',
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '0000 0000 00',
+                    email: 'julie@localhost.com',
+                },
+            ]      
+        }
+    },
+
+};
+</script>
