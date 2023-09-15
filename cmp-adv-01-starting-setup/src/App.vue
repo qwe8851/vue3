adf4d1520f1ad84d33a85c7bc5e6b8b9ca02e747
qwe8851 <dgh07027@naver.com>
2023-09-15 18:41:16 +0900

    refactor: 전역컴포넌트 지역컴포넌트로 변경

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
+    <the-header />
+    <badge-list />
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -11,7 +11,21 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  // components: {
+  //   'the-header': TheHeader,
+  //   'badge-list': BadgeList,
+  //   'user-info': UserInfo,
+  // },
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
