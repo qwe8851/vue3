560d9afe212049dfe05c28384ce32af6c4c4953a
qwe8851 <dgh07027@naver.com>
2023-09-16 17:00:16 +0900

    feat: scoped slot으로 item바인딩할 html태그 보내기
    default slot의 경우 template 생략 가능
    +) 아마 props명을 케밥케이스로 작성했으면 받을 때 카멜케이스로 받아야하는듯 하다
    slotProps['another-props']로 받았더니 불러오질 못해서 케밥케이스로 작성했더니 동작함.

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -7,6 +7,12 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <CourseGoals #default="slotProps">
+      <!-- <template #default="slotProps"> -->
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProps }}</p>
+      <!-- </template> -->
+    </CourseGoals>
   </div>
 </template>
 
@@ -14,17 +20,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
-  // components: {
-  //   'the-header': TheHeader,
-  //   'badge-list': BadgeList,
-  //   'user-info': UserInfo,
-  // },
   components: {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {
